bb3aa122044375f4dcbaa1bfd5d3e5aac84bd8e9
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-06 05:59:53 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,6 +23,8 @@ def bonus
   #code your solution here:
 
   
+    #epic_tragedy[:capulet][:patriarch][:matriarch][:heroine][:status] = "dead"  #["name"]["age"]["status"] = "dead"
+  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
