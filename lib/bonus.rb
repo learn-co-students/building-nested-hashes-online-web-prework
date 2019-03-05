ea3d6579b492d1fd13e230e33ac34d0922ed62e9
David Stewart <davidstewart@gmail.com>
2019-03-05 02:25:43 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def bonus
   epic_tragedy = {
    :montague => {
@@ -22,8 +23,11 @@ def bonus
 
   #code your solution here:
 
-  
-
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy
+#binding.pry
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  
 end
+#binding.pry
