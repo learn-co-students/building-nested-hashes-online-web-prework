db5e132bf429a5e3bf8cfff638acfa65da31ac92
Rene Cocom <fasnimo@gmail.com>
2019-05-10 05:42:22 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,17 +4,16 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
+      :hero_friends => [ friend_one = {name:"Benvolio", age:"17", attitude:"worried"}, friend_two = {name:"Mercutio", age:"18", attitude:"hot-headed"}
+        
       ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
+      :heroine_friends => [ heroine_friends1 = {name:"Steven", age:"30", attitude:"confused"}, heroine_friends2 = {name:"Nurse", age:"44", attitude:"worried"}
+        ]
    }
 }
 
