1e58e372aff88e175bb77d73ae7caafd0fd8b5c3
Jack Atherton <jackrrye@gmail.com>
2019-05-29 22:03:15 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {name: "Mercutio", age: "18", attutide: "hot-headed"},
+        {name: "Benvolio", age: "17", attitude: "worried"}
+        
       ]
    }, 
    :capulet => {
@@ -14,10 +17,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
-
-  
-
 end
